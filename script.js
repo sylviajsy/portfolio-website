479acbc1923c150820d87ee03fc92b6b948f37bd
Siyi Ji <sylviajsyy@gmail.com>
2025-11-12 13:50:11 -0600

    Modal Window

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Modal Window
+    const modal = document.getElementById("contactModal");
+    const contactbtn = document.getElementById("contact-btn");
+    const span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    contactbtn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+});
