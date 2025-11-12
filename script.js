ccba2287cebc73602342472de6f97840c63a62e7
Siyi Ji <sylviajsyy@gmail.com>
2025-11-12 14:10:16 -0600

    Search function with highlights

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Nav Bar Search function
+    const form = document.getElementById("navBar-search-form");
+     form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        const newInput = document.getElementById('search-input');
+        const query = document.getElementById('search-input').value.trim().toLowerCase();
+
+        if (!query) return;
+
+        const elements = document.querySelectorAll("h1, h2, h3, h4, p, li, footer");
+        let found = false;
+        for (const i of elements){
+            if (i.textContent.toLowerCase().includes(query.toLowerCase())){
+                found = true;
+                i.scrollIntoView({ behavior: "smooth", block: "center" });
+                const regex = new RegExp(`(${query})`, "gi");
+                i.innerHTML = i.innerHTML.replace(regex, `<mark>$1</mark>`);
+                break;
+            }
+        }
+            if (!found){alert(`No Match found for "${query}"`);}
+            newInput.value = "";
+    });
     // Modal Window
     const modal = document.getElementById("contactModal");
     const contactbtn = document.getElementById("contact-btn");
     const span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal
+        // When the user clicks on the button, open the modal
     contactbtn.onclick = function() {
     modal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the modal
+        // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
     modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
+        // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
